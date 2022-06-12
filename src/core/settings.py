2b52d0f60069cae199e3da93fe8974bf290ed08b
Edwin Aguilar <edwinaguilar1a@gmail.com>
2022-06-12 05:28:21 -0500

    raa

--- src/core/settings.py
+++ src/core/settings.py
@@ -17,12 +17,12 @@ load_dotenv()
 class Settings():
 
     def __init__(self):
-        # self.app_name = os.environ['APP_NAME']
-        # self.db_name = os.environ['DB_NAME']
-        # self.db_port = int(os.environ['DB_PORT'])
-        # self.db_host = os.environ['DB_HOST']
-        # self.db_user = os.environ['DB_USER']
-        # self.db_password = os.environ['DB_PASSWORD']
-        self.app_name = 'xd'
+        self.app_name = os.environ['APP_NAME']
+        self.db_name = os.environ['DB_NAME']
+        self.db_port = int(os.environ['DB_PORT'])
+        self.db_host = os.environ['DB_HOST']
+        self.db_user = os.environ['DB_USER']
+        self.db_password = os.environ['DB_PASSWORD']
+        # self.app_name = 'xd'
 
 settings = Settings()
