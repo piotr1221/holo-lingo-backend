c1a727360bddca0d9033a76c803c339e7af7a7d2
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2022-06-18 11:29:19 -0500

    feat: search endpoint

--- src/api/v1/lessons.py
+++ src/api/v1/lessons.py
@@ -2,21 +2,22 @@ from src.api.v1.contracts.lesson import LessonPost
 from src.core.schemas.Lessons import Lesson
 from fastapi import APIRouter
 from src.core.settings import settings
+import json
 
 lessons_router = APIRouter()
 
 
 @lessons_router.get('/lessons/search/{term}')
-async def get_lessons(term):
-    return {
-        'message': 'obtener lecciones'
-    }
+def search_lesson(term: str):
+    return json.loads(Lesson.objects(title__icontains=term).to_json())
+
 
 @lessons_router.post('/lessons/create')
 async def post_lessons(lesson: LessonPost):
     print(lesson)
-    new_lesson=Lesson(title=lesson.title,description=lesson.description,example_video=lesson.example_video,category_name=lesson.category_name)
+    new_lesson = Lesson(title=lesson.title, description=lesson.description,
+                        example_video=lesson.example_video, category_name=lesson.category_name)
     new_lesson.save()
     return {
-        'message':"Lesson saved successfully"
+        'message': "Lesson saved successfully"
     }
