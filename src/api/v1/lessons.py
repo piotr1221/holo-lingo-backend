59c677230031dd7d572d6227af5b6d3f265b05be
unknown <erickp1502@gmail.com>
2022-06-18 11:24:15 -0500

    feat: create lesson endpoint

--- src/api/v1/lessons.py
+++ src/api/v1/lessons.py
@@ -1,3 +1,5 @@
+from src.api.v1.contracts.lesson import LessonPost
+from src.core.schemas.Lessons import Lesson
 from fastapi import APIRouter
 from src.core.settings import settings
 
@@ -8,4 +10,13 @@ lessons_router = APIRouter()
 async def get_lessons(term):
     return {
         'message': 'obtener lecciones'
+    }
+
+@lessons_router.post('/lessons/create')
+async def post_lessons(lesson: LessonPost):
+    print(lesson)
+    new_lesson=Lesson(title=lesson.title,description=lesson.description,example_video=lesson.example_video,category_name=lesson.category_name)
+    new_lesson.save()
+    return {
+        'message':"Lesson saved successfully"
     }
--- src/core/schemas/Lessons.py
+++ src/core/schemas/Lessons.py
@@ -1,16 +1,10 @@
-from curses import meta
-from unicodedata import category
 from mongoengine import *
 import datetime as dt
+
 
 class Lesson(Document):
     title = StringField(required=True,max_length=100)
     description=StringField(max_length=255)
     example_video=StringField(max_length=255)
     date_created = DateTimeField(default=dt.datetime.utcnow,help_text='date the user was created')
-    category=ReferenceField(Category)
-    meta={'allow_inheritance':True}
-
-
-class Category(Document):
-  name= StringField(required=True,max_length=100)
+    category_name=StringField(max_length=50)
