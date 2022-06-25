862086d5a6261df3ba107227c096696a9e8ebcc1
Edwin Aguilar <edwinaguilar1a@gmail.com>
2022-06-25 16:45:59 -0500

    ra

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from typing import Union
 from fastapi import FastAPI
 from mangum import Mangum
-# from starlette.middleware.sessions import SessionMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
 from src.core.settings import settings
 from src.core.db.db import init_database, shutdown_database
-# from src.api.v1.auth import auth_router
-# from src.api.v1.info import info_router
-# from src.api.v1.lessons import lessons_router
+from src.api.v1.auth import auth_router
+from src.api.v1.info import info_router
+from src.api.v1.lessons import lessons_router
 
 app = FastAPI()
 
-# app.add_middleware(SessionMiddleware, secret_key="some-random", https_only=True)
+app.add_middleware(SessionMiddleware, secret_key="some-random", https_only=True)
 
-# app.include_router(auth_router)
-# app.include_router(info_router)
-# app.include_router(lessons_router)
+app.include_router(auth_router)
+app.include_router(info_router)
+app.include_router(lessons_router)
 
 
 @app.get('/v1/info')
--- src/api/v1/info.py
+++ src/api/v1/info.py
@@ -3,14 +3,14 @@ from src.core.settings import settings
 
 info_router =APIRouter(prefix="/v1")
 
-# @info_router.get('/')
-# async def index():
-#     return {
-#         'api_version': 'v1'
-#     }
+@info_router.get('/')
+async def index():
+    return {
+        'api_version': 'v1'
+    }
 
-# @info_router.get('/info')
-# async def info():
-#     return {
-#         'app_name': settings.app_name
-#     }
+@info_router.get('/info')
+async def info():
+    return {
+        'app_name': settings.app_name
+    }
