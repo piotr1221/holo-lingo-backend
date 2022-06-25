2361e43a0cdcef3fed36450aa2f65538f17716c3
Edwin Aguilar <edwinaguilar1a@gmail.com>
2022-06-25 14:00:43 -0500

    ra

--- src/core/schemas/AppUser.py
+++ src/core/schemas/AppUser.py
@@ -1,17 +1,17 @@
-from mongoengine import *
-import datetime as dt
-from passlib.hash import pbkdf2_sha256
+# from mongoengine import *
+# import datetime as dt
+# from passlib.hash import pbkdf2_sha256
 
-class AppUser(Document):
-    name = StringField(required=True,max_length=128)
-    password = StringField(required=True)
-    date_created = DateTimeField(default=dt.datetime.utcnow,help_text='date the user was created')
-    issuer=StringField(required=True,max_length=256)
-    email=StringField(required=True, max_length=128)
+# class AppUser(Document):
+#     name = StringField(required=True,max_length=128)
+#     password = StringField(required=True)
+#     date_created = DateTimeField(default=dt.datetime.utcnow,help_text='date the user was created')
+#     issuer=StringField(required=True,max_length=256)
+#     email=StringField(required=True, max_length=128)
 
-    def save(self, *args, **kwargs):
-        self.password = pbkdf2_sha256.hash(self.password)
-        super(AppUser, self).save(*args, **kwargs)
+#     def save(self, *args, **kwargs):
+#         self.password = pbkdf2_sha256.hash(self.password)
+#         super(AppUser, self).save(*args, **kwargs)
         
-    def val_password(self, out_password: str):
-        return pbkdf2_sha256.hash(out_password,self.password)
+#     def val_password(self, out_password: str):
+#         return pbkdf2_sha256.hash(out_password,self.password)
