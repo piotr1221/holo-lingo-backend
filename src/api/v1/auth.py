bfc8f1aa7bfbb0bd3e121eb70d1784f50814666b
unknown <erickp1502@gmail.com>
2022-07-08 22:33:32 -0500

    mucho cuidao

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -36,7 +36,8 @@ async def login_via_email(user:ClassicLoginUser):
     result = target.val_password(user.password)
     if result:
         return JSONResponse({
-            'message': 'user authenticated'
+            'message': 'user authenticated',
+            'data':target
         },status_code=200)
     else:
         return JSONResponse({
